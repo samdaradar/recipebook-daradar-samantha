72b16af81e8aa0f3d0f887ad633e809e44728af5
Sam Daradar <samantha.noryn.daradar@student.ateneo.edu>
2026-03-03 22:31:00 +0800

    staged admin changes

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Recipe, RecipeIngredient, Ingredient
 # Register your models here.
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline]
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
